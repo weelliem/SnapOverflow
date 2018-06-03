f812624eef1179cafc013651f5e594a32bb68ea1
weelliem <williamhong@live.com.au>
2018-06-03 19:38:34 +1000

    added comments for models

--- app/src/main/java/com/mad/snapoverflow/model/FourmModel.java
+++ app/src/main/java/com/mad/snapoverflow/model/FourmModel.java
@@ -1,5 +1,24 @@
+/*
+ * Copyright 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      Author: 11025400
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+
 package com.mad.snapoverflow.model;
 
+
+/* this model contains the getter and setters for the fourm fragement activity and also allows the link between firebase and the application */
 public class FourmModel {
 
 
